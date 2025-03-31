46d550e13d62658a87fc488451484da54d47e7c4
Neeraj Jain <neerajjain1392@gmail.com>
2025-03-31 12:55:04 +0530

    Initial commit

---
+++ screener-in-layout-fixer/popup.js
@@ -0,0 +1,5 @@
+document.getElementById('apply-now').addEventListener('click', () => {
+  chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
+    chrome.tabs.sendMessage(tabs[0].id, {action: "applyFixes"});
+  });
+});
